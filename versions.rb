cfb799f39e2f8cad79b94caaadcb5716b6830b82
Jonathan Dance <jd@wuputah.com>
2010-09-30 19:15:13 -0400

    a 1.9.2 hotfix - hope it doesnt break in other versions

--- versions.rb
+++ versions.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'cgi'
 require 'erb'
 
+enable :run
+
 get '/' do
   headers 'Cache-Control' => 'public, max-age=3600'
   ENV['DATABASE_URL'] = ENV['DATABASE_URL'].sub(/:(?!\/\/)[^@]*/, ':xxxxxxxxxxxxxxxxx')
